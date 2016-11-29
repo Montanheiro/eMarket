9dfe58638566dae4afd4a8cf514b40d0feaf2dcd
Montanheiro <lucasmontanheiro10@gmail.com>
2016-11-29 12:31:34 -0200

    pagina de submenu com icones

--- web/js/menuItemsCtrl.js
+++ web/js/menuItemsCtrl.js
@@ -1,7 +1,7 @@
     angular.module('spa').controller('MenuItemsCtrl', function($scope) {
                             //{name:'exemple', link:'http://exemple.com or #/exemple', icon:'assets-cache.js name'},
         $scope.MenuItems = [{name:'Início', link:'#/', icon:'ic_chevron_right_24px'},
-                            {name:'Financeiro', link:'#/financeiro', icon:'favorite'}]
+                            {name:'Financeiro', link:'#/financeiro', icon:'ic_attach_money_24px'}]
                             //{name:'Compras', link:'#/compras', icon:'favorite'},
                             //{name:'Vendas', link:'#/vendas', icon:'favorite'},
                             //{name:'Estoque', link:'#/estoque', icon:'favorite'}]
--- web/old/js/main.js
+++
@@ -1,6 +0,0 @@
-angular.module("principal", ['ngMaterial']);
-angular.module('principal')
-	.controller("principalCtrl", function ($scope){
-	
-	$scope.tituloMenu = "Esse é o titulo do menu";
-});
