7f1d254d70e682d233ce668e2555017124ba41cd
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-02 01:49:16 -0200

    iniciando cadastro de clientes

--- web/js/financeiroCadClienteCtrl.js
+++ web/js/financeiroCadClienteCtrl.js
@@ -2,4 +2,17 @@
 angular.module('spa')
 	.controller('financeiroCadClienteCtrl', ['$scope', '$log', function($scope, $log){
 		$scope.name = 'Cadastro de Clientes';
+
+		$scope.estados = [
+
+			{id: 1, nome: 'São Paulo'},
+			{id: 2, nome: 'Goias'},
+			{id: 3, nome: 'Tocantins'}
+
+		];
+
+		$scope.selecionarEstado = [
+			{id: 1, nome: 'São Paulo'}
+		];
+
 }]);
