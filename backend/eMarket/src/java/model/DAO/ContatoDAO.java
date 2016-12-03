251f9a043aa25a69cfb2f60839e56cc80f6e2c24
Bárbara Carrijo <barbarahcarrijo@gmail.com>
2016-12-02 22:20:31 -0200

    Correção classe ContatoDAO

--- backend/eMarket/src/java/model/DAO/ContatoDAO.java
+++ backend/eMarket/src/java/model/DAO/ContatoDAO.java
@@ -17,14 +17,16 @@ public class ContatoDAO {
     private ContatoDAO() {
     }
     
+    //Metodo CREATE esta OK, testado e funcionando
     public static int create(Contato c) {
         try {
             Statement stm
                     = BancoDados.createConnection().
                             createStatement();
             String sql
-                    = "INSERT INTO contato (`Nome`) VALUES ('"
-                    + c.getTelefone() + "')";
+                    = "INSERT INTO contato (`Telefone`, `Pessoa_id`) VALUES ('"
+                    + c.getTelefone() + "','"
+                    + c.getId() + "')";
 
             stm.execute(sql, Statement.RETURN_GENERATED_KEYS);
             ResultSet rs = stm.getGeneratedKeys();
@@ -32,119 +34,103 @@ public class ContatoDAO {
             int key = rs.getInt(1);
             c.setId(key);
             return key;
-
         } catch (SQLException ex) {
             Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return 0;
 
     }
-    
+    //Metodo RETREAVE esta OK, testado e funcionando
     public static Contato retreave(int id) {
-        
         try {
             Statement stm
                     = BancoDados.createConnection().
                             createStatement();
-
-            //SELECT * FROM emark.cargo where id =1;
-            String sql = "SELECT * FROM emark.contatos where id =" + id;
+            String sql = "SELECT * FROM contato where id =" + id;
             ResultSet rs = stm.executeQuery(sql);
             rs.next();
-            
             return new Contato(rs.getInt("id"),
-                    rs.getString("nome"),
-                    rs.getInt("pessoaId"));
-            
+                    rs.getString("Telefone"),
+                    rs.getInt("Pessoa_id"));
         } catch (SQLException ex) {
             Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
-        
     }
-    
+
+    //Metodo RETREAVE esta OK, testado e funcionando
     public static Contato retreaveByPessoa(int pessoaId) {
         try {
             Statement stm
                     = BancoDados.createConnection().
                             createStatement();
-            String sql = "SELECT * FROM emark.contatos where Pessoa_id =" + pessoaId;
+            String sql = "SELECT * FROM contato where Pessoa_id =" + pessoaId;
             ResultSet rs = stm.executeQuery(sql);
             if (rs.next()) {
-                
                 return new Contato(
                         rs.getInt("id"),
-                        rs.getString("telefone"),
-                        rs.getInt("pessoa_id"));
+                        rs.getString("Telefone"),
+                        rs.getInt("Pessoa_id"));
             }
         } catch (SQLException ex) {
             Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
+
+    //Metodo RETREAVE esta OK, testado e funcionando
     public static ArrayList<Contato> retreaveAll() {
-        
+
         try {
             Statement stm
                     = BancoDados.createConnection().
                             createStatement();
-            String sql = "SELECT * FROM emark.contatos";
+            String sql = "SELECT * FROM contato";
             ResultSet rs = stm.executeQuery(sql);
-            
             ArrayList<Contato> c = new ArrayList<>();
-            
             while (rs.next()) {
                 c.add(new Contato(
                         rs.getInt("id"),
-                        rs.getString("nome"),
+                        rs.getString("Telefone"),
                         rs.getInt("Pessoa_id")));
             }
-            
             rs.next();
             return c;
-            
         } catch (SQLException ex) {
             Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
-        
     }
-    
-    public static void delete(Contato c) {
-        try {
-            Statement stm
-                    = BancoDados.createConnection().
-                            createStatement();
 
-            //DELETE FROM `emark`.`contatos` WHERE `id`='4';
-            String sql = "DELETE FROM `emark`.`contatos` WHERE `id`="
-                    + c.getId();
-            
-            stm.execute(sql);
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
+    //Metodo UPDATE esta OK, testado e funcionando
     public static void update(Contato c) {
         try {
             Statement stm
                     = BancoDados.createConnection().
                             createStatement();
-
-            //UPDATE `emark`.`contatos` SET `Nome`='Atendente' WHERE `id`='4';
-            String sql = "UPDATE `emark`.`contatos` SET "
-                    + "`Nome`='" + c.getTelefone()
+            String sql = "UPDATE contato SET "
+                    + "`Telefone`='" + c.getTelefone()
                     + "' WHERE `id`= "
                     + c.getId();
-            
             stm.execute(sql);
-            
+        } catch (SQLException ex) {
+            Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+//Metodo DELETE esta OK, testado e funcionando    
+    public static void delete(Contato c) {
+        try {
+            Statement stm
+                    = BancoDados.createConnection().
+                            createStatement();
+            String sql = "DELETE FROM contato WHERE `id`="
+                    + c.getId();
+            stm.execute(sql);
         } catch (SQLException ex) {
             Logger.getLogger(ContatoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
+
 }
