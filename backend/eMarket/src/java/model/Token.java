a03cc35ab73e771bc2b3b9c5bc1f4804bbb48e51
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-10 21:39:20 -0200

    Geração e verificação de Token OK

---
+++ backend/eMarket/src/java/model/Token.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.security.Key;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ *
+ * @author lucas
+ */
+public class Token {
+    
+    public String GerarToken(String issuer, String subject, long ttlMillis) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(new TokenKey().getTokenkey());
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setIssuedAt(now)
+                                    .setSubject(subject)
+                                    .setIssuer(issuer)
+                                    .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+        long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+    
+    //Sample method to validate and read the JWT
+    public boolean VerificarToken(String jwt) throws Exception {
+
+                      
+        //This line will throw an exception if it is not a signed JWS (as expected)       
+        try {
+            Claims claims = Jwts.parser()         
+           .setSigningKey(DatatypeConverter.parseBase64Binary(new TokenKey().getTokenkey()))
+           .parseClaimsJws(jwt).getBody();
+            
+            System.out.println("Subject: " + claims.getSubject());
+            System.out.println("Issuer: " + claims.getIssuer());
+            System.out.println("Expiration: " + claims.getExpiration());
+            
+            return true;
+        
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException | UnsupportedJwtException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+       
+    }
+}
