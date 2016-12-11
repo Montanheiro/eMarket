15d581bf3d33e28a22fd19571806355145e53f28
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-11 11:50:50 -0200

    Reiniciar conexão com o banco de dados

--- backend/eMarket/src/java/model/DAO/BancoDados.java
+++ backend/eMarket/src/java/model/DAO/BancoDados.java
@@ -1,43 +1,110 @@
 package model.DAO;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+///**
+// *
+// * @author Bárbara
+// */
+//public class BancoDados {
+//
+//    private static Connection conn;
+//    
+//    private BancoDados(){
+//        
+//    }
+//    
+//    public static Connection createConnection() {
+//        if (conn != null){
+//            return conn;
+//        }
+//        
+//        try {
+//            Class.forName(BDConfig.DRIVER);
+//            conn = DriverManager.getConnection(
+//                        BDConfig.URL,
+//                        BDConfig.USR, 
+//                        BDConfig.PWD);           
+//            return conn;
+//        } catch (ClassNotFoundException | SQLException ex) {
+//            Logger.getLogger(BancoDados.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        return null;
+//        
+//    }
+//    
+//    
+//    
+//}
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+//TUTORIAL: http://www.devmedia.com.br/criando-uma-conexao-java-mysql-server/16753
+
+import java.sql.Connection; 
+import java.sql.DriverManager; 
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author Bárbara
- */
-public class BancoDados {
-
-    private static Connection conn;
+ 
+//Início da classe de conexão//
+public class BancoDados { 
     
-    private BancoDados(){
-        
+    public static String status = "Não conectou...";
+ 
+    //Método Construtor da Classe//
+        public BancoDados() {
     }
-    
-    public static Connection createConnection() {
-        if (conn != null){
-            return conn;
-        }
-        
+ 
+    //Método de Conexão//
+    public static java.sql.Connection createConnection() {
+        Connection connection;          //atributo do tipo Connection
         try {
-            Class.forName(BDConfig.DRIVER);
-            conn = DriverManager.getConnection(
-                        BDConfig.URL,
-                        BDConfig.USR, 
-                        BDConfig.PWD);           
-            return conn;
-        } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(BancoDados.class.getName()).log(Level.SEVERE, null, ex);
+            // Carregando o JDBC Driver padrão
+            String driverName = "com.mysql.jdbc.Driver";                        
+            Class.forName(driverName);
+
+            connection = DriverManager.getConnection(BDConfig.URL, BDConfig.USR, BDConfig.PWD);
+
+            //Testa sua conexão//  
+            if (connection != null) {
+                status = ("STATUS--->Conectado com sucesso!");
+            } 
+            else {
+                status = ("STATUS--->Não foi possivel realizar conexão");
+            }return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
+            System.out.println("O driver expecificado nao foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            //Não conseguindo se conectar ao banco
+            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+            return null;
         }
-        
-        return null;
-        
     }
-    
-    
-    
+ 
+    //Método que retorna o status da sua conexão//
+    public static String statusConection() {
+        return status;
+    }
+
+   //Método que fecha sua conexão//
+    public static boolean FecharConexao() {
+ 
+        try {
+            BancoDados.createConnection().close(); 
+            return true;
+ 
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+  
+    //Método que reinicia sua conexão//
+    public static java.sql.Connection ReiniciarConexao() {
+        FecharConexao();
+        return BancoDados.createConnection();
+    }
 }
