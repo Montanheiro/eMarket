08eeb74a337a1a058aecaa855a6e55fc59482381
Dione Alvino <dionegtba@gmail.com>
2016-12-12 12:45:13 -0200

    ProdutoResource + token (testem)

--- backend/eMarket/src/java/controller/ProdutosResource.java
+++ backend/eMarket/src/java/controller/ProdutosResource.java
@@ -1,21 +1,26 @@
 package controller;
 
 import com.google.gson.Gson;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import model.DAO.ProdutoDAO;
 import model.Produto;
+import model.Token;
 
 /**
  * REST Web Service
  *
- * @author Bárbara
+ * @author Dione
  */
 @Path("produtos")
 public class ProdutosResource {
@@ -29,40 +34,57 @@ public class ProdutosResource {
     public ProdutosResource() {
     }
 
-    /**
-     * Retrieves representation of an instance of controller.ProdutosResource
-     * @return an instance of java.lang.String
-     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+    @Path("/consultar")
+    public String consultarTodos(@QueryParam("token") String t) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
         Gson gson = new Gson();
-        
-/*        ArrayList<Produto> cs = new ArrayList<>();
-        
-        Produto pd1 = new Produto("Abobrinha", 1, 15);
-        Produto pd2 = new Produto("Chuchu", 1, 15);
-        Produto pd3 = new Produto("Cocada", 1, 15);
-        Produto pd4 = new Produto("Beterraba", 1, 15);
-        
-        cs.add(pd1);
-        cs.add(pd2);
-        cs.add(pd3);
-        cs.add(pd4);
-*/        
-        
-        
-        return gson.toJson("Produto");
-        
-    
+        ArrayList<Produto> produto = ProdutoDAO.retreaveAll();
+         return gson.toJson(produto);
+                
     }
-
-    /**
-     * PUT method for updating or creating an instance of ProdutosResource
-     * @param content representation for the resource
-     */
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/consultarid")
+    public String  consultarId(@QueryParam("token") String t, @QueryParam("id")int id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Produto p = ProdutoDAO.retreave(id);
+        return gson.toJson(p);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/inserir")
+    public int inserir(@QueryParam("token")String t, String data) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Produto p = gson.fromJson(data, Produto.class);
+        ProdutoDAO.create(p); 
+        return 200;
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/deletar")
+    public int deletar(@QueryParam("token")String t, String id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Produto p = gson.fromJson(id, Produto.class);
+        ProdutoDAO.delete(p);
+        return 200;
+    }    
+        
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @Path("/atualizar")
+    public void alterar (@QueryParam("token")String t,String data) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Produto p = gson.fromJson(data, Produto.class);
+            ProdutoDAO.update(p);
     }
+   
 }
