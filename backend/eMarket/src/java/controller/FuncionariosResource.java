bb9af112918723d6a1883c67c26ba97a35000d6b
Dione Alvino <dionegtba@gmail.com>
2016-12-12 12:36:18 -0200

    funcionariosResource + token (testem)

--- backend/eMarket/src/java/controller/FuncionariosResource.java
+++ backend/eMarket/src/java/controller/FuncionariosResource.java
@@ -8,15 +8,19 @@ package controller;
 import com.google.gson.Gson;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import model.DAO.FuncionarioDAO;
 import model.Funcionario;
+import model.Token;
 
 /**
  * REST Web Service
@@ -35,22 +39,57 @@ public class FuncionariosResource {
     public FuncionariosResource() {
     }
 
-    @GET
+   @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/consultar")
-    public String consultarTodos() throws SQLException{
+    public String consultarTodos(@QueryParam("token") String t) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
         Gson gson = new Gson();
-        ArrayList<Funcionario> funcionario = FuncionarioDAO.retreaveAll();
-         return gson.toJson(funcionario);
+        ArrayList<Funcionario> f = FuncionarioDAO.retreaveAll();
+         return gson.toJson(f);
                 
     }
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/consultarid")
-    public String  consultarId(@QueryParam("id")int id) throws SQLException {
+    public String  consultarId(@QueryParam("token") String t, @QueryParam("id")int id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
         Gson gson = new Gson();
         Funcionario f = FuncionarioDAO.retreave(id);
         return gson.toJson(f);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/inserir")
+    public int inserir(@QueryParam("token")String t, String data) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Funcionario f = gson.fromJson(data, Funcionario.class);
+        FuncionarioDAO.create(f); 
+        return 200;
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/deletar")
+    public int deletar(@QueryParam("token")String t, String id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Funcionario f = gson.fromJson(id, Funcionario.class);
+        FuncionarioDAO.delete(f);
+        return 200;
+    }    
+        
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/atualizar")
+    public void alterar (@QueryParam("token")String t,String data) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Funcionario f = gson.fromJson(data, Funcionario.class);
+            FuncionarioDAO.update(f);
     }
     
 }
