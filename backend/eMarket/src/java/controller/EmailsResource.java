5f28c94cb97afd98e5f4fb4ca5d1e26a0b9f45ba
Dione Alvino <dionegtba@gmail.com>
2016-12-12 12:08:21 -0200

    emailResource + token (testar)

---
+++ backend/eMarket/src/java/controller/EmailsResource.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.google.gson.Gson;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import model.DAO.EmailDAO;
+import model.Email;
+import model.Token;
+
+/**
+ * REST Web Service
+ *
+ * @author dione
+ */
+@Path("emails")
+public class EmailsResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of EmailsResource
+     */
+    public EmailsResource() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/consultar")
+    public String consultarTodos(@QueryParam("token") String t) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        ArrayList<Email> cliente = EmailDAO.retreaveAll();
+         return gson.toJson(cliente);
+                
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/consultarid")
+    public String  consultarId(@QueryParam("token") String t, @QueryParam("id")int id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Email e = EmailDAO.retreave(id);
+        return gson.toJson(e);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/inserir")
+    public int inserir(@QueryParam("token")String t, String data) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Email e = gson.fromJson(data, Email.class);
+        EmailDAO.create(e); 
+        return 200;
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/deletar")
+    public int deletar(@QueryParam("token")String t, String id) throws SQLException, Exception {
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Email e = gson.fromJson(id, Email.class);
+        EmailDAO.delete(e);
+        return 200;
+    }    
+        
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/atualizar")
+    public void alterar (@QueryParam("token")String t,String data) throws SQLException, Exception{
+        if (!new Token().VerificarToken(t)) throw new Exception("token invalido");
+        Gson gson = new Gson();
+        Email e = gson.fromJson(data, Email.class);
+            EmailDAO.update(e);
+    }
+}
