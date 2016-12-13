8e732bc3e5b8a12b2104c16678d87af90a413657
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-13 05:38:14 -0200

    Filtro para conexão Ajax
    Adiciona o allow origin ao cabeçalho da resposta http

---
+++ backend/eMarket/src/java/controller/FiltroConexao.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FiltroConexao implements Filter{
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+            FilterChain chain) throws IOException, ServletException {
+      
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("Request" + request.getMethod());
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,OPTIONS");
+        resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+   
+        chain.doFilter(servletRequest, servletResponse);            
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+}
