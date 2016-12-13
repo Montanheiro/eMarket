b3abd7eb65861b678339172c9dfadea71ca13443
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-13 14:23:15 -0200

    Aceitando o token no header pelo FiltroConexao

--- backend/eMarket/src/java/controller/FiltroConexao.java
+++ backend/eMarket/src/java/controller/FiltroConexao.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FiltroConexao implements Filter{
@@ -23,7 +22,7 @@ public class FiltroConexao implements Filter{
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         resp.addHeader("Access-Control-Allow-Origin","*");
         resp.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,OPTIONS");
-        resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+        resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, token");
 
         resp.setStatus(HttpServletResponse.SC_OK);
    
