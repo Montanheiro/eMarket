38496954818bd4c7266ec3aadae701e34c0635c8
Montanheiro <lucasmontanheiro10@gmail.com>
2016-12-13 15:06:27 -0200

    Cargos funcionando 100% no front

--- web/painel/js/adminCargosEditarCtrl.js
+++ web/painel/js/adminCargosEditarCtrl.js
@@ -1,42 +1,74 @@
 angular.module('spa')
-	.controller('adminCargosEditarCtrl', [ '$scope', '$rootScope', '$routeParams', '$http', '$location',
-	function($scope, $rootScope,$routeParams, $http, $location){
+	.controller('adminCargosEditarCtrl', [ '$scope', '$rootScope', '$routeParams', '$http', '$location', '$mdToast',
+	function($scope, $rootScope,$routeParams, $http, $location, $mdToast){
     	$scope.name = 'Editar cargo';
 
+        $scope.cargo = $location.search();
+
+        console.log($scope.cargo);
+
         $scope.cancelar = function(){
-            $location.path('administracao/cadastros/cargos');
+            $location.path('administracao/cadastros/cargos').search({});
         };
 
-    	$scope.editarCargo = function(){
+    	$scope.editar = function(){
             var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");     
-            if(!token) {
+            if(token) {
                 $http({ 
-                        url: $rootScope.api + '/eMarket/webresources/cargos', 
+                        url: $rootScope.api + '/cargos/atualizar', 
                         dataType: 'json', 
                         method:'PUT',
-                        headers: {'x-access-token': token,'Content-Type': 'application/json'},
+                        headers: {'token': token,'Content-Type': 'application/json'},
                         data: { 
-                            'descricao': $scope.nome
+                            'id': $scope.cargo.id,
+                            'nome': $scope.cargo.nome
                         }
                     }).success(function (response) {                        
-                        console.log(response.msg);
+                        console.log(response);
                         $mdToast.show(
                             $mdToast.simple()
-                                .content(response.msg)
+                                .content("Editado com sucesso!")
                                 .hideDelay(3000)
                         );
-                        $location.path('/administracao/cadastros/cargos');
-
+                        $location.path('/administracao/cadastros/cargos').search({});
                     }).error(function (response) {
-                        console.log(response.msg);
-                        $mdToast.show(
-                            $mdToast.simple()
-                                .content(response.msg)
-                                .hideDelay(3000)
-                        );
+                        console.log(response);
+                        alert("erro ao editar ou no token");
+                        window.location = "/#/login";
                     });
             }else{
             	alert("sem token");
+                window.location = "/#/login";
+            }
+        };
+
+        $scope.excluir = function(){
+            var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");     
+            if(token) {
+                $http({ 
+                        url: $rootScope.api + '/cargos/deletar', 
+                        dataType: 'json', 
+                        method:'POST',
+                        headers: {'token': token,'Content-Type': 'application/json'},
+                        data: { 
+                            'id': $scope.cargo.id,
+                            'nome': $scope.cargo.nome
+                        }
+                    }).success(function (response) {                        
+                        console.log(response);
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .content("Excluído com sucesso!")
+                                .hideDelay(3000)
+                        );
+                        $location.path('/administracao/cadastros/cargos').search({});
+                    }).error(function (response) {
+                        console.log(response);
+                        alert("erro ao excluir ou no token");
+                        window.location = "/#/login";
+                    });
+            }else{
+                alert("sem token");
                 window.location = "/#/login";
             }
         };
--- web/painel/js/adminCargosInserirCtrl.js
+++ web/painel/js/adminCargosInserirCtrl.js
@@ -1,40 +1,37 @@
 angular.module('spa')
-	.controller('adminCargosInserirCtrl', [ '$scope', '$rootScope', '$routeParams', '$http', '$location',
-	function($scope, $rootScope,$routeParams, $http, $location){
+	.controller('adminCargosInserirCtrl', [ '$scope', '$rootScope', '$routeParams', '$http', '$location', '$mdToast',
+	function($scope, $rootScope,$routeParams, $http, $location, $mdToast){
     	$scope.name = 'Inserir novo cargo';
 
         $scope.cancelar = function(){
-            $location.path('administracao/cadastros/cargos');
+            $location.path('administracao/cadastros/cargos').search({});
         };
 
 
     	$scope.inserirCargo = function(){
             var token = sessionStorage.getItem("user_session") || localStorage.getItem("user_session");     
-            if(!token) {
+            if(token) {
                 $http({ 
-                        url: $rootScope.api + '/eMarket/webresources/cargos', 
+                        url: $rootScope.api + '/cargos/inserir', 
                         dataType: 'json', 
                         method:'POST',
-                        headers: {'x-access-token': token,'Content-Type': 'application/json'},
+                        headers: {'token': token,'Content-Type': 'application/json'},
                         data: { 
-                            'descricao': $scope.nome
+                            'nome': $scope.nome
                         }
                     }).success(function (response) {                        
-                        console.log(response.msg);
+                        console.log(response);
                         $mdToast.show(
                             $mdToast.simple()
-                                .content(response.msg)
+                                .content("Inserido com sucesso!")
                                 .hideDelay(3000)
                         );
-                        $location.path('/administracao/cadastros/cargos');
+                        $location.path('/administracao/cadastros/cargos').search({});
 
                     }).error(function (response) {
-                        console.log(response.msg);
-                        $mdToast.show(
-                            $mdToast.simple()
-                                .content(response.msg)
-                                .hideDelay(3000)
-                        );
+                        console.log(response);
+                        alert("erro ao inserir ou no token");
+                        window.location = "/#/login";
                     });
             }else{
             	alert("sem token");
